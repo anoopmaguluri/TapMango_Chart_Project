8daad5d4608c0b04d3797c3ba692292ac22cce66
anoopmaguluri <anoopmaguluri9@gmail.com>
2024-11-13 03:25:52 -0500

    updated dashboard componet to display all three charts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AreaChartComponent } from '../area-chart/area-chart.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -6,6 +6,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
+import { DataService } from '../../services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -21,4 +23,33 @@ import { MatSelectModule } from '@angular/material/select';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+  charts: Array<{
+    dataServiceMethod: () => Observable<any>;
+    title: string;
+    chartId: string;
+  }> = [];
+  gridColumns = 2;
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.charts = [
+      {
+        dataServiceMethod: this.dataService.getCombinedData.bind(this.dataService),
+        title: 'Combined Data Chart',
+        chartId: 'combinedDataChart',
+      },
+      {
+        dataServiceMethod: this.dataService.getSteadyGrowthData.bind(this.dataService),
+        title: 'Steady Growth Chart',
+        chartId: 'steadyGrowthChart',
+      },
+      {
+        dataServiceMethod: this.dataService.getSeasonalFluctuationData.bind(this.dataService),
+        title: 'Seasonal Fluctuation Chart',
+        chartId: 'seasonalFluctuationChart',
+      },
+
+    ];
+  }
+} 
